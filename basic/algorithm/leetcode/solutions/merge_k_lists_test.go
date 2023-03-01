a01709645fb135f11ad79e212312e97fbf3ab8a0
dijiarong <dijiarong@163.com>
2023-03-01 10:34:45 +0800

    fix

--- basic/algorithm/leetcode/solutions/merge_k_lists_test.go
+++ basic/algorithm/leetcode/solutions/merge_k_lists_test.go
@@ -1,18 +1,17 @@
 package solutions
 
 import (
-	"gostudy/basic/algorithm/listnode"
 	"testing"
 )
 
 func TestMergeKLists(t *testing.T) {
-	lists := []*listnode.ListNode{}
-	node6 := &listnode.ListNode{Val: 5, Next: nil}
-	node5 := &listnode.ListNode{Val: 3, Next: node6}
-	node4 := &listnode.ListNode{Val: 1, Next: node5}
-	node3 := &listnode.ListNode{Val: 6, Next: nil}
-	node2 := &listnode.ListNode{Val: 4, Next: node3}
-	node1 := &listnode.ListNode{Val: 2, Next: node2}
+	lists := []*ListNode{}
+	node6 := &ListNode{Val: 5, Next: nil}
+	node5 := &ListNode{Val: 3, Next: node6}
+	node4 := &ListNode{Val: 1, Next: node5}
+	node3 := &ListNode{Val: 6, Next: nil}
+	node2 := &ListNode{Val: 4, Next: node3}
+	node1 := &ListNode{Val: 2, Next: node2}
 	lists = append(lists, node1)
 	lists = append(lists, node4)
 	MergeKLists(lists)
